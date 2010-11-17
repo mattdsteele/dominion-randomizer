6a73a2453c71f0c12b78319d06ab75a33b3d86b6
Matt Steele <orphum@gmail.com>
2010-11-16 18:57:15 -0600

    adding some tests

--- spec/dominion-randomizer_spec.rb
+++ spec/dominion-randomizer_spec.rb
@@ -1,5 +1,28 @@
-describe "something" do
-  it "does something" do
-    fail
+class Randomizer
+  def get_random_cards
+    []
+  end
+end
+
+class Card
+end
+
+describe "Card" do
+  it "can be constructed" do
+    card = Card.new
+    card.should_not.nil?
+  end
+end
+
+describe "Randomizer" do
+  it "can be constructed" do
+    randomizer = Randomizer.new
+    randomizer.should_not == nil
+  end
+
+  it "has some cards in it" do
+    randomizer = Randomizer.new
+    cards = randomizer.get_random_cards
+    cards.should == []
   end
 end
