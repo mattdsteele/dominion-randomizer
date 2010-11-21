68530b2c08f75386a07a84f82ffc274c49a9b361
Matt Steele <orphum@gmail.com>
2010-11-21 16:59:52 -0600

    some basic randomization

--- spec/dominion-randomizer_spec.rb
+++ spec/dominion-randomizer_spec.rb
@@ -1,10 +1,16 @@
 class Randomizer
+  def random_cards
+    [{},
+    {}]
+  end
+
   def get_random_cards
-    []
+    random_cards.sort_by {rand}
   end
 end
 
 class Card
+  
 end
 
 describe "Card" do
@@ -23,6 +29,21 @@ describe "Randomizer" do
   it "has some cards in it" do
     randomizer = Randomizer.new
     cards = randomizer.get_random_cards
-    cards.should == []
+    cards.should be_an(Array)
+    cards.size.should > 0
+  end
+
+  it "should not return the same list each time" do
+    randomizer = Randomizer.new
+    first_cards = randomizer.get_random_cards
+    second_cards = randomizer.get_random_cards
+    first_cards.object_id.should_not === second_cards.object_id
+  end
+
+  it "should only contain the objects from random_cards" do
+    randomizer = Randomizer.new
+    cards = randomizer.random_cards
+    random_set = randomizer.get_random_cards
+    random_set.each {|i| cards.should include(i) }
   end
 end
